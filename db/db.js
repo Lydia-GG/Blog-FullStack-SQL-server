148868c1a2c99741f72f11f9524bcf74c751ae3f
Lydia Samir <lolyeg83@hotmail.com>
2024-02-14 20:25:41 +0100

    connected database on render

--- db/db.js
+++ db/db.js
@@ -2,9 +2,16 @@ import mysql from 'mysql';
 import dotenv from 'dotenv';
 dotenv.config();
 export const db = mysql.createConnection(
+  // {
+  //   host: process.env.HOST,
+  //   user: process.env.USER,
+  //   password: process.env.PASSWORD,
+  //   database: process.env.DATABASE,
+  // },
   {
-    host: process.env.HOST,
-    user: process.env.USER,
+    hostname: process.env.HOSTNAME,
+    port: process.env.PORT,
+    username: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
   },
